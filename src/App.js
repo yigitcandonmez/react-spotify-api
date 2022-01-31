e017b9c1b20e01c8fdbe1803011b1f6c1fdd8b11
yigitcandonmez <muratcandonmez123@gmail.com>
2022-01-31 23:28:38 +0300

    react-spotify-api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-function App() {
+const CLIENT_ID = "e3de7f0e9b3b4375b1406400d76dd66c";
+const CLIENT_SECRET = "5c78dba0a50b4ea680e1153cfacf30aa";
+
+const App = () => {
+  const [userToken, setUserToken] = useState();
+  const [track, setTrack] = useState();
+
+  const handleClick =  () => {
+     axios('https://accounts.spotify.com/api/token', {
+      headers: {
+        'Content-Type' : 'application/x-www-form-urlencoded',
+        'Authorization' : 'Basic ' + btoa(CLIENT_ID + ':' + CLIENT_SECRET)      
+      },
+      data: 'grant_type=client_credentials',
+      method: 'POST'
+    })
+    .then(tokenResponse => {      
+      setUserToken(tokenResponse.data.access_token);
+    });
+  }
+
+  const handleResponse =  () => {
+     axios('https://api.spotify.com/v1/tracks/6kLCHFM39wkFjOuyPGLGeQ?market=US', {
+      method: 'GET',
+      headers: { 'Authorization' : 'Bearer ' + userToken}
+    })
+    .then (genreResponse => {        
+      setTrack(genreResponse.data.album);
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleClick}>Spotify</button>
+      <br/>
+      <br/>
+      {userToken}
+      <br/>
+      <br/>
+      <button onClick={handleResponse} disabled={!userToken}>Response</button>
+      <hr/>
+      {track ? track.name : ""}
     </div>
   );
-}
+};
 
 export default App;
